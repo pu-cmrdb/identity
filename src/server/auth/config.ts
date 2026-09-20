5e9cd8e58a5fc40c89fedd22bf32e4848241975b
Kamiya <kamiya@kamiya.app>
2026-09-20 12:48:52 +0800

    feat: app resource aud
    Signed-off-by: Kamiya <kamiya@kamiya.app>

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -42,6 +42,7 @@ export const auth = betterAuth({
       silenceWarnings: {
         oauthAuthServerConfig: true,
       },
+      resources: ['https://app.cmrdb.cs.pu.edu.tw'],
     }),
     username({
       usernameNormalization: normalizeUsername,
